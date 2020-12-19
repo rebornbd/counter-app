d1f66af07d4d8e14f5a6e034fde29156b6b8f775
rebornbd <d.dipta.cse@gmail.com>
2020-12-19 23:45:55 +0600

    vidly (jsx + renderingList + conditionalRendering + eventHandling + updateState)

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import Counter from "./components/counter";
-
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <Counter />,
+  <App />,
   document.getElementById('root')
 );
 
