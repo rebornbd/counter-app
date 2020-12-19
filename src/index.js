dce3d7671c2e7df5b35306d418cf11db2aaac26c
rebornbd <d.dipta.cse@gmail.com>
2020-12-19 14:06:22 +0600

    specifyingChildren

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Counter from "./components/counter";
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <Counter />,
   document.getElementById('root')
 );
 
