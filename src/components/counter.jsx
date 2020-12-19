c73acf5383ff3dbb4838c87cea44cc47f78e9902
rebornbd <d.dipta.cse@gmail.com>
2020-12-19 15:18:16 +0600

    Setting Attributes in jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        imageUrl: "https://picsum.photos/200"
+    };
+
+    mystyle = {
+        fontSize: 10,
+        fontWeight: 'bold'
     };
 
     render() {
         return (
             <React.Fragment>
-                <span>{this.formateCount()}</span>
+                <img style={{ width:100, height:50, margin:10 }} src={this.state.imageUrl} alt=""/>
+                <span style={ this.mystyle } className="badge badge-warning">{this.formateCount()}</span>
                 <button className="btn btn-primary">Increment</button>
             </React.Fragment>
         );
