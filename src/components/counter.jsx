96206ea20b43b4d20b319b2b59b12e51b149c51a
rebornbd <d.dipta.cse@gmail.com>
2020-12-20 23:34:49 +0600

    Composing Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    // method: 01 (Binding Event Handlers)
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-
-    // handleIncrement() {
-    //     console.log("clicked", this);
-    // }
-
     state = {
         count: 0,
-        tags: ["tag1", "tag2", "tag3"]
     };
 
     render() {
         return (
-            <React.Fragment>
+            <div className="my-2">
                 <span className={ this.getBadgeClasses() }>{this.formateCount()}</span>
-                <button onClick={ this.handleIncrement } className="btn btn-primary">Increment</button>
-                <ul>{ this.getRenderingList() }</ul>
-            </React.Fragment>
+                <button onClick={ this.handleIncrement } className="btn btn-primary btn-sm">Increment</button>
+            </div>
         );
     };
 
-    // method: 02 (Binding Event Handlers)
     handleIncrement = () => {
         const myCount = this.state.count;
         this.setState({ count: myCount+1 });
     };
-
-    getRenderingList() {
-        let newList = this.state.tags.map(tag => <li key={tag}>{ tag.toLocaleUpperCase() }</li>);
-        return newList;
-    }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
